d14f96a8c2f6970ad2375727e3357a4cfc44f56d
marialuizam <159549561+marialuizam@users.noreply.github.com>
2024-10-08 11:52:22 -0300

    rgrg

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Química',
+    'Metais pesados:',
+    'Motorhead, Metallica, Decalius, Blade and Bath'
+)
+
+criaCartao(
+    'Sei lá',
+    'Oque esse cartão faz?',
+    ''
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
